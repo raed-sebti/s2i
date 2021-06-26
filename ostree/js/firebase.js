2c51991acd4b95b5d883cbe54762d1903e74ac5c
raed-sebti <sebti.raed@gmail.com>
2021-06-26 04:49:00 +0100

    first commit

---
+++ ostree/js/firebase.js
@@ -0,0 +1,15 @@
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyANgEEeRzRKv4v7b0eBu-Xeprsn1wtSXuI",
+    authDomain: "my-k8s-labs.firebaseapp.com",
+    databaseURL: "https://my-k8s-labs.firebaseio.com",
+    projectId: "my-k8s-labs",
+    storageBucket: "my-k8s-labs.appspot.com",
+    messagingSenderId: "273462121138",
+    appId: "1:273462121138:web:7c7860bb3830571b17afab",
+    measurementId: "G-4XRP9K5GTG"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
