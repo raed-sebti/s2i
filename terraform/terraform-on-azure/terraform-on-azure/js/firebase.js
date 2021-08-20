ebc6c78e621ea1b0718295b93ab536b50fc5d55d
raed-sebti <sebti.raed@gmail.com>
2021-08-20 04:14:14 +0100

    first commit

---
+++ terraform/terraform-on-azure/terraform-on-azure/js/firebase.js
@@ -0,0 +1,14 @@
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyD2J4iwnRwts0VmsgWGlhRoZWEf3kUCW40",
+    authDomain: "terraform-hashicorp-labs.firebaseapp.com",
+    projectId: "terraform-hashicorp-labs",
+    storageBucket: "terraform-hashicorp-labs.appspot.com",
+    messagingSenderId: "480677415689",
+    appId: "1:480677415689:web:6a44d8a1b0b857d9b321d8",
+    measurementId: "G-6LZE517YVY"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
